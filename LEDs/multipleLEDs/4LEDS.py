4034047f830c9cfec9ec506b9df052336a0a98d4
okomojacob <jaylabs2030@gmail.com>
2021-03-30 01:54:30 +0300

    usee 1 GND

--- LEDs/multipleLEDs/4LEDS.py
+++ LEDs/multipleLEDs/4LEDS.py
@@ -1,18 +1,20 @@
 """
-Alternate the flashing of the LEDs
+Alternate the flashing of the LEDs,
+Ensure you only use one GND for all GPIO connected pins.
 """
 from gpiozero import LED
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 import time
 
-tme = 0.5
+tme = 0.3
 #tme = float(input("Sleep for how many Seconds: "))
 led1 = LED(4)
 led2 = LED(17)
 led3 = LED(27)
 led4 = LED(22)
 
+"""fucntion to alternate the blinking of LEDs"""
 def alternateFlash():
     while True:
         led1.on()
