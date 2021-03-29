e5e94f4ef0abb4392b1cebb11fc8e00e5806f084
okomojacob <jaylabs2030@gmail.com>
2021-03-30 02:01:32 +0300

    usee 1 GND

--- LEDs/multipleLEDs/4LEDS.py
+++ LEDs/multipleLEDs/4LEDS.py
@@ -28,6 +28,8 @@ def alternateFlash():
         led3.off()
         time.sleep(tme)
         led3.on()
+        
+        led1.on(); led2.on(); led3.on(); led4.on();
 """          
         led4.off()
         time.sleep(tme)
