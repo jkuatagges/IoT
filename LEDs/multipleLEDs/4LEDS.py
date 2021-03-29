d40210ca3422e22b94c28fc865a46d978701a147
okomojacob <jaylabs2030@gmail.com>
2021-03-30 02:05:48 +0300

    tweaking patterns

--- LEDs/multipleLEDs/4LEDS.py
+++ LEDs/multipleLEDs/4LEDS.py
@@ -28,8 +28,14 @@ def alternateFlash():
         led3.on()
         time.sleep(tme)
         led3.off()
+        time.sleep(tme)
         
-        led1.on(); led2.on(); led3.on();
+        led4.on()
+        time.sleep(tme)
+        led4.off()
+        time.sleep(tme)
+        
+        led1.on(); led2.on(); led3.on(); led4.on()
 """          
         led4.off()
         time.sleep(tme)
