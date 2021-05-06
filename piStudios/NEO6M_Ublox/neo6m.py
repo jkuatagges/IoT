035e7698434d500e211a56ebd51c21b0853093c6
okomojacob <jaylabs2030@gmail.com>
2021-05-06 20:47:42 +0300

    :bug:

--- piStudios/NEO6M_Ublox/neo6m.py
+++ piStudios/NEO6M_Ublox/neo6m.py
@@ -2,13 +2,15 @@ import string import pynmea2
 import serial
 import time
 
-while True: 
-    port = “/dev/ttyAMAO”
-    ser = serial.Serial(port,baudrate=9600,timeout=0.5)
-    dataout = pynmea2.NMEAStreamReader()
-    newdata = ser.readline()
+def runNeo():
+    while True: 
+        port = “/dev/ttyAMAO”
+        ser = serial.Serial(port,baudrate=9600,timeout=0.5)
+        dataout = pynmea2.NMEAStreamReader()
+        newdata = ser.readline()
 
-if newdata[0:6]== “$GPRMC”:
+
+if newdata[0:6] == “$GPRMC”:
     newmsg = pynmea2.parse(newdata)
     lat = newmsg.latitude
     lng = newmsg.longitude
