f5e25e063c2e8b4a2bbc5b4148df8807a95c5627
Jacob Okello Okomo <jaylabs2030@gmail.com>
2021-05-13 16:55:37 +0300

    :satellite: gps code updated, less :bug:

--- piStudios/NEO6M_Ublox/neo6m.py
+++ piStudios/NEO6M_Ublox/neo6m.py
@@ -1,18 +1,24 @@
-import string import pynmea2
+import pynmea2
+import string 
 import serial
 import time
+import os
+
+getLocation = True
 
 def runNeo():
-    while True: 
+    while getLocation: 
         port = “/dev/ttyAMAO”
         ser = serial.Serial(port,baudrate=9600,timeout=0.5)
         dataout = pynmea2.NMEAStreamReader()
         newdata = ser.readline()
+        
+    print("Getting Latitude and Longitude")
 
-
-if newdata[0:6] == “$GPRMC”:
-    newmsg = pynmea2.parse(newdata)
-    lat = newmsg.latitude
-    lng = newmsg.longitude
-    gps = “Latitude = " +str(lat) + “and Longitude=" + str(lng)
-    print(gps)
+    #if newdata[0:6] == “$GPGGA”:
+    if newdata[0:6] == “$GPRMC”:
+        newmsg = pynmea2.parse(newdata)
+        lat = newmsg.latitude
+        lng = newmsg.longitude
+        gps = “Latitude = " +str(lat) + “and Longitude=" + str(lng)
+        print(gps)
