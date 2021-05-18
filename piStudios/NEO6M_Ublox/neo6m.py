4a3568910281ae22f003a5d9c21bcb6e684fb279
okomojacob <jaylabs2030@gmail.com>
2021-05-18 10:55:10 +0300

    :bug:, :bug:, and more :bug:, some removed, some alive

--- piStudios/NEO6M_Ublox/neo6m.py
+++ piStudios/NEO6M_Ublox/neo6m.py
@@ -1,20 +1,33 @@
+from signal import signal, SIGTERM, SIGHUP, pause
 import pynmea2
 import string 
 import serial
 import time
 
-getLocation = True
+def safe_exit(signum, frame):
+    exit(1)
+signal(SIGTERM, safe_exit)
+signal(SIGHUP, safe_exit)
 
-while getLocation: #run forever
-    port = "/../../dev/ttyAMA0" #Nav to where NMEA was initiated(/)
-    ser = serial.Serial(port,baudrate=9600,timeout=0.5)
-    dataout = pynmea2.NMEAStreamReader()
-    newdata = ser.readline() #readline from the serial connection
+try:     
+    getLocation = True
+    while getLocation: #run forever
+        port = "/../../dev/ttyAMA0" #Nav to where NMEA was initiated(/)
+        ser = serial.Serial(port,baudrate=9600,timeout=0.5)
+        dataout = pynmea2.NMEAStreamReader()
+        newdata = ser.readline() #readline from the serial connection
 
-    #if newdata[0:6] == "$GPGGA, GPRMC":
-    if newdata[0:6] == "$GPGLL": #get GPsGlobal data as Lat, Long
-        newmsg = pynmea2.parse(newdata)
-        lat = newmsg.latitude
-        lng = newmsg.longitude
-        gps = "Lat = " +str(lat) + ", Long =" + str(lng)
-        print(gps)
+        #if newdata[0:6] == "$GPGGA, GPRMC":
+        if newdata[0:6] == "$GPGLL": #get GPsGlobal data as Lat, Long
+            newmsg = pynmea2.parse(newdata)
+            lat = newmsg.latitude
+            lng = newmsg.longitude
+            gps = "Lat = " +str(lat) + ", Long =" + str(lng)
+            print(gps)
+            
+except KeyboardInterrupt:
+    pass
+
+finally:
+    pass
+
